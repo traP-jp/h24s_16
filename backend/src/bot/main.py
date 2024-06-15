7ebd2e12309096c7daafe29b33e5804ff2e516ce
trasta <trasta298@gmail.com>
2024-06-16 04:05:24 +0900

    🚧 wip

--- backend/src/bot/main.py
+++ backend/src/bot/main.py
@@ -1,6 +1,7 @@
 import asyncio
 import json
 import os
+import re
 
 from aiotraq import AuthenticatedClient
 from aiotraq_bot import TraqHttpBot
@@ -21,12 +22,27 @@ response = TraqMessageManager(bot, bot_access_token, base_url=base_url, base_cli
 client = AuthenticatedClient(base_url=base_url, token=bot_access_token)
 
 
-async def component(am: TraqMessage, payload: MessageCreatedPayload | BotMessageStampsUpdatedPayload):
+async def on_stamp(am: TraqMessage, payload: BotMessageStampsUpdatedPayload):
     am.write(f"```json\n{json.dumps(jsonable_encoder(payload), indent=2)}\n```")
     # long task
     with am.spinner():
         await asyncio.sleep(3)
     am.write(":done: Done!")
+
+
+async def on_reply(am: TraqMessage, payload: MessageCreatedPayload):
+    # 正規表現で先頭の `!{.*} ` を削除
+    text = payload.message.text
+    text = re.sub(r"^!\\{[^}]*\\}", "", text).lstrip()
+
+    # 正規表現で https://q.trap.jp/messages/{message_id} を抽出
+    message_text = re.search(r"https://q.trap.jp/messages/([0-9a-f-]+)", text)
+    if message_text is None:
+        # 引用投稿がなかったとき
+        am.write("投稿を引用すると、タスクに追加できるよ！")
+        return
+    message_id = message_text.group(1)
+    am.write(f"この投稿をタスクに追加するよ！\nhttps://q.trap.jp/messages/{message_id}")
 
 
 @bot.event("BOT_MESSAGE_STAMPS_UPDATED")
@@ -38,15 +54,13 @@ async def on_stamps_updated(payload: BotMessageStampsUpdatedPayload) -> None:
         return
     channel_id: str = res.channel_id
 
-    await response(component, channnel_id=channel_id, payload=payload)
+    await response(on_stamp, channnel_id=channel_id, payload=payload)
 
 
 @bot.event("MESSAGE_CREATED")
 async def on_message_created(payload: MessageCreatedPayload) -> None:
     channel_id = payload.message.channelId
-    # message = payload.message.plainText
-
-    await response(component, channnel_id=channel_id, payload=payload)
+    await response(on_reply, channnel_id=channel_id, payload=payload)
 
 if __name__ == "__main__":
     bot.run(port=8080)
