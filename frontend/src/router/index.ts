a56dc52e1b93cb3ac1047fcafee55c44a91b6e8b
Pugma <maronmarondelicious@gmail.com>
2024-06-15 09:33:40 +0900

    ファイル名を更新する

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -1,5 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/taskAdd.vue'
+import HomeView from '../views/TempFileName.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
