73c3155b21ca31f01fbe419f8a36075bdc3c9396
Aster <shnogm@gmail.com>
2024-06-15 12:26:44 +0900

    とりあえず各欄をつくった

--- frontend/src/views/TaskAdd.vue
+++ frontend/src/views/TaskAdd.vue
@@ -3,7 +3,36 @@ import TheWelcome from '../components/TheWelcome.vue'
 </script>
 
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <div id="app">
+    <v-app>
+    <v-main>
+      <v-layout>
+        <v-card width="80em" variant="outlined" class="mx-auto">
+          <v-container>
+            <v-text-field label="タスク名">
+
+            </v-text-field>
+            <v-text-field label="アサイン先(@で指定)">
+
+            </v-text-field>
+            <v-text-field label="項目">
+
+            </v-text-field>
+            <v-text-field label="内容" class="h-30">
+
+            </v-text-field>
+            <v-text-field label="タグ">
+
+            </v-text-field>
+            <v-row class="justify-center">
+              <v-btn color="primary" variant="outlined">
+                作成
+              </v-btn>
+            </v-row>
+          </v-container>
+        </v-card>
+      </v-layout>
+    </v-main>
+    </v-app>
+  </div>
 </template>
