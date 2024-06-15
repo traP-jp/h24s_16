62310e75dfa2d53b842320eedd4de5a957d6b3f5
Pugma <maronmarondelicious@gmail.com>
2024-06-15 09:34:14 +0900

    ファイル名をもとに戻した

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -1,5 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/TempFileName.vue'
+import HomeView from '../views/TaskAdd.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
