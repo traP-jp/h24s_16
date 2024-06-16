a10f5942869dc44f680a3ff894e7d7fd6719a1b2
mtaku3 <m.taku3.1222@gmail.com>
2024-06-16 05:05:00 +0000

    fix create task

--- backend/src/tasq/main.py
+++ backend/src/tasq/main.py
@@ -136,16 +136,9 @@ def create_task(new_task: CreateTaskReqDTO, username: Annotated[str, Depends(tra
     traq_user = get_traq_user_from_name(username)
     if not new_task.group_id in traq_user.groups:
         raise HTTPException(status_code=404, detail="ユーザーがこのグループに所属していません")
-    labels = []
-    for label_id in label_ids:
-        db_label = crud.read_label(db, label_id)
-        if not db_label:
-            raise HTTPException(status_code=404, detail="ラベルが存在しません")
-        if db_label.group.id != new_task.group_id:
-            raise HTTPException(status=400, detail="ラベルが該当グループに属していません")
-        labels.append(db_label)
-    db_crud_task = schemas.TaskCreate(title=new_task.title, content=new_task.content, message_id=new_task.message_id, due_date=new_task.due_date, group_id=new_task.group_id, labels=labels)
+    db_crud_task = schemas.TaskCreate(title=new_task.title, content=new_task.content, message_id=new_task.message_id, due_date=new_task.due_date, group_id=new_task.group_id)
     db_crud_task = crud.create_task(db, db_crud_task)
+    crud.create_task_label(db, task, label_ids)
     for db_user_id in new_task.assigned_user_ids:
         if crud.read_user(db, db_user_id) is None:
             crud.create_user(db, schemas.UserCreate(id=db_user_id))
