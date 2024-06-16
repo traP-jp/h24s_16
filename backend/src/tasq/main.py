aa641866b6fd9e82a9dd4550defe83a3350bd71e
eki <i@eki.moe>
2024-06-16 11:41:05 +0900

    Remove bot token

--- backend/src/tasq/main.py
+++ backend/src/tasq/main.py
@@ -25,7 +25,7 @@ app.add_middleware(
 
 trao_scheme = APIKeyHeader(name="X-Forwarded-User", scheme_name="traO")
 
-traqapi_config = traqapi.Configuration(access_token="Uc5ZrONZChvN8myK1jaiXYkNEtxvES70MVHt")
+traqapi_config = traqapi.Configuration(access_token="")
 traqapi_config.verify_ssl = False
 traqapi_client = traqapi.ApiClient(configuration=traqapi_config)
 traqUserApi = traqapi.UserApi(api_client=traqapi_client)
