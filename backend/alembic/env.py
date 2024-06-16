51700e4eb9d18e980f62c282f2a08b78f1ec0dd8
mtaku3 <m.taku3.1222@gmail.com>
2024-06-16 02:53:26 +0000

    なおれおい

--- backend/alembic/env.py
+++ backend/alembic/env.py
@@ -11,13 +11,12 @@ from tasq.repository.models import Base
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
-section = config.config_ini_section
 
 if os.getenv("PYTHON_ENV") == "production":
     DATABASE_URL = os.environ["DB_URL"]
 else:
     DATABASE_URL = "sqlite:///./test.db"
-config.set_section_option(section, "DB_URL", DATABASE_URL)
+config.set_main_option("sqlalchemy.url", DATABASE_URL)
 
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
