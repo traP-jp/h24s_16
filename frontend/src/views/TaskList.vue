8562559db0ed37e123f89e55f24d535d88e6f6e9
Pugma <maronmarondelicious@gmail.com>
2024-06-16 09:32:09 +0900

    タスク一覧のデザインを修正

--- frontend/src/views/TaskList.vue
+++ frontend/src/views/TaskList.vue
@@ -71,23 +71,9 @@ const tasks = ref<TaskDetails[]>([])
   height: 100%;
   padding: 60px 30px; /* Adjust this value to match the height of your header */
   overflow: auto;
-}
-
-.sidebar li {
-  margin-bottom: 20px; /* Adjust this value to increase or decrease the space */
-}
-
-.task-container {
-  border: none;
-  box-shadow: 0px 4px 8px rgba(0, 0, 0, 0.1);
-  border-radius: 10px; /* 枠を丸くする */
-  padding: 10px;
-  margin: 15px;
-  // margin-left: 230px;
-  // margin-right: 30px;
-  /* margin-right: 30px; */
-  background-color: white; /* 背景色を変更する */
-  /* width: calc(100% - 230px);  */
+  li {
+    margin-bottom: 20px; /* Adjust this value to increase or decrease the space */
+  }
 }
 
 .task-title {
@@ -99,60 +85,10 @@ ul {
   list-style-type: none; /* 中黒を消す */
 }
 
-/* 詳細ボタンのスタイル */
-.task-details {
-  display: flex;
-  justify-content: space-between; /* 左右にコンテンツを分散 */
-  align-items: center; /* 中央揃え */
-}
-
-.detail-button {
-  padding: 8px 16px; /* ボタンの内側の余白 */
-  color: $color-primary; /* ボタンのテキスト色 */
-  border: none; /* ボーダーを削除 */
-  cursor: pointer; /* ホバー時にカーソルをポインターにする */
-  margin-bottom: 3px; /* ボタンと詳細情報の間の余白 */
-}
-
-.detail-button:hover {
-  text-decoration: underline; /* 下線を表示 */
-}
-
-.close-button {
-  padding: 8px 16px; /* ボタンの内側の余白 */
-  color: #6aa2b4; /* ボタンのテキスト色 */
-  cursor: pointer; /* ホバー時にカーソルをポインターにする */
-}
-
-.close-button:hover {
-  text-decoration: underline; /* 下線を表示 */
-}
-
-/* グループ選択 */
-.groupSelect-button {
-  font-weight: bold; /* 太文字にする */
-}
-
-.groupSelect-button:hover {
-  text-decoration: underline; /* 下線を表示 */
-}
 .pageContents {
   height: calc(100% - 5rem);
   display: grid;
   grid-template-columns: 1fr 5fr;
   align-content: center;
-}
-
-@media (min-width: 1024px) {
-  .about {
-    min-height: 100vh;
-    display: flex;
-    align-items: center;
-  }
-  .task-container {
-    margin-left: 230px; /* サイドバーの幅 + 30px の余白を確保 */
-    margin-right: 30px;
-    /* width: calc(100% - 230px); 全体の幅からサイドバーの幅を引く */
-  }
 }
 </style>
